c07446b6887ad6b2fcc14be18c02903047958262
alaa <alaothman110@gmail.com>
2023-04-17 20:36:44 +0100

    check point redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment ,incrementByAmount } from "./redux/counter";
 
 function App() {
+  const count = useSelector((state) => state.counter.value  ) 
+  const dispatch= useDispatch()
   return (
-   <div></div>
+   <div>
+    <button onClick={()=> dispatch (increment())}>increment</button>
+    <span>{count}</span>
+    <button onClick={()=> dispatch(decrement()) }>decrement</button>
+    <button  onClick={()=> dispatch(incrementByAmount(10))}>increment by 10 </button>
+   </div>
    
   );
 }
